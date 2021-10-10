d266fd8dd7b256b6d26097bf870fa993beed3d71
dunsparce9 <66077935+dunsparce9@users.noreply.github.com>
2021-10-10 19:45:55 +0300

    First working version

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,16 +1,23 @@
-﻿using Autopraisal.Models;
+﻿using Autopraisal;
+using Autopraisal.Models;
 using OreCalc.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
+using System.Windows.Media.Animation;
 
 namespace OreCalc
 {
@@ -102,9 +109,34 @@ namespace OreCalc
         List<EveItem> items = new List<EveItem>();
         Regex itemName = new Regex(@"[^\t]*");
         Regex itemQty = new Regex(@"\t\d{1,3}(\.\d{1,3})?(\.\d{1,3})?");
+        Storyboard sb = new Storyboard();
+        ThicknessAnimation slide = new ThicknessAnimation();
+        bool SlidingDown = false;
+        NameValueCollection outgoingQueryString = HttpUtility.ParseQueryString(String.Empty);
         public MainWindow()
         {
             InitializeComponent();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            slide.BeginTime = new TimeSpan(0);
+            slide.SetValue(Storyboard.TargetProperty, mainGrid);
+            Storyboard.SetTargetProperty(slide, new PropertyPath(MarginProperty));
+
+            slide.From = new Thickness(0, 25, 0, 0);
+            slide.To = new Thickness(0, 0, 0, 0);
+            slide.Duration = new Duration(TimeSpan.FromSeconds(0.4));
+
+            sb.Children.Add(slide);
+            sb.Completed += Sb_Completed;
+
+        }
+
+        private void Sb_Completed(object sender, EventArgs e)
+        {
+            if (SlidingDown)
+            {
+                Visibility = Visibility.Collapsed;
+            }
         }
 
         private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
@@ -118,6 +150,7 @@ namespace OreCalc
             Process p = GetActiveProcess();
             if (p.ProcessName == "exefile" && p.MainWindowTitle.StartsWith("EVE - "))
             {
+                items.Clear();
                 using (StringReader reader = new StringReader(Clipboard.GetText()))
                 {
                     string line;
@@ -128,32 +161,68 @@ namespace OreCalc
                 }
                 if (items.Count > 0)
                 {
-                    tbItem.Text = items.Count > 1 ? "(multiple items)" : items[0].Name;
-                    var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://evepraisal.com/appraisal/structured.json");
-                    httpWebRequest.ContentType = "application/json";
-                    httpWebRequest.Method = "POST";
-                    httpWebRequest.UserAgent = "Autopraisal/0.0.1a (github.com/dunsparce9/autopraisal)";
-                    using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                    tbItem.Text = items.Count > 1 ? "(multiple items)" : items[0].Quantity.ToString() + " x " + items[0].Name;
+                    var result = Appraise(Clipboard.GetText());
+                    tbPrice.Text = String.Format(result.appraisal.totals.buy.ToString("N"));
+                    Visibility = Visibility.Hidden;
+                    UpdateLayout();
+                    Rect desktopWorkingArea = SystemParameters.WorkArea;
+                    Left = desktopWorkingArea.Right - Width;
+                    Top = desktopWorkingArea.Bottom - Height;
+                    Visibility = Visibility.Visible;
+                    Slide(true);
+                    Task.Delay(2000).ContinueWith(_ =>
                     {
-                        streamWriter.Write(JsonSerializer.Serialize(new Appraisal("jita", Clipboard.GetText())));
-                    }
-
-                    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-                    {
-                        var result = streamReader.ReadToEnd();
-                    }
+                        Slide(false);
+                    });
                 }
             }
-        }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            var desktopWorkingArea = SystemParameters.WorkArea;
-            Left = desktopWorkingArea.Right - Width;
-            Top = desktopWorkingArea.Bottom - Height;
+            Result Appraise(string clipboardText)
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://evepraisal.com/appraisal.json?persist=no");
+                httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+                httpWebRequest.Method = "POST";
+                httpWebRequest.UserAgent = "Autopraisal/0.0.1a (github.com/dunsparce9/autopraisal)";
+                outgoingQueryString.Add("market", "jita");
+                outgoingQueryString.Add("price_percentage", "90");
+                outgoingQueryString.Set("raw_textarea", Clipboard.GetText());
+                string postdata = outgoingQueryString.ToString();
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(postdata);
+                }
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    string response = streamReader.ReadToEnd();
+                    return JsonSerializer.Deserialize<Result>(response);
+                }
+            }
+
+            void Slide(bool up)
+            {
+                SlidingDown = !up;
+                if (up)
+                {
+                    App.Current.Dispatcher.Invoke((Action)delegate
+                    {
+                        slide.From = new Thickness(0, 25, 0, 0);
+                        slide.To = new Thickness(0, 0, 0, 0);
+                        sb.Begin();
+                    });
+                }
+                else
+                {
+                    App.Current.Dispatcher.Invoke((Action)delegate
+                    {
+                        slide.From = new Thickness(0, 0, 0, 0);
+                        slide.To = new Thickness(0, 25, 0, 0);
+                        sb.Begin();
+                    });
+                }
+            }
+
         }
     }
-
-
 }
