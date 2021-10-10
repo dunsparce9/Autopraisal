95752e2b9f129e0a2ed0ef3226915d7aa07aac26
dunsparce9 <66077935+dunsparce9@users.noreply.github.com>
2021-10-10 17:42:39 +0300

    Remove unused namespaces

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,24 +4,13 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OreCalc
 {
